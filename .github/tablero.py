221dc21274f464dcd7ce01585aa5631cece1851d
Nicolas Almenara <n.almenara@alumno.um.edu.ar>
2025-08-12 09:23:57 -0300

    arreglo errores

--- .github/tablero.py
+++ .github/tablero.py
@@ -8,8 +8,3 @@ class Tablero:
             ["", "", ""],
             ["", "", ""]
         ]
-
-    def poner_la_ficha(self, fil, col, ficha):
-        if self.contenedor[fil][col] != "":
-            raise PosOcupadaException("La posición ya está ocupada")
-        self.contenedor[fil][col] = ficha
